6a47fd91f3d0c9699dcf0dc341c7daef9b53a9ce
rjadyshkom <sergey@rjadysh.com>
2022-02-18 23:27:50 +0300

    sergey fix: refactoring

---
+++ utils/parser.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function getMonitorStatus(URL) {
+    return await axios.get(URL)
+        .then((res) => {
+            const $ = cheerio.load(res.data);
+            const model = $('div.article').find('strong').text();
+            const available = $('div.article').find('span:last-child').text();
+            // date
+            const today = new Date();
+            const year = today.getFullYear() + '-' +
+                String((today.getMonth() + 1)).padStart(2, '0') + '-' +
+                String(today.getDate()).padStart(2, '0');
+            const time = today.getHours() + ':' +
+                String(today.getMinutes()).padStart(2, '0');
+            // telegram
+            return `${year}, ${time} LG UltraFine <b>${model}</b> — ${available}`;
+        })
+        .catch(err => console.log(err))
+}
+
+module.exports = { getMonitorStatus };
