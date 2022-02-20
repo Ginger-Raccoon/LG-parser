949e059c2c0d647ede4a31c95b54f5f975b93e2c
rjadyshkom <sergey@rjadysh.com>
2022-02-20 12:27:15 +0300

    sergey feat: history

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const { Telegraf } = require('telegraf');
-const { serverLogger, monitorsLogger } = require('./utils/logger');
+const { serverLogger, monitorsLogger, historyLogger } = require('./utils/logger');
 const { getMonitorStatus } = require('./utils/parser');
+const tagsRemover = require('./helpers/tagsRemover');
+const fs = require('fs');
 
 const {
-    PORT = 3000,
+    PORT,
     BIG_MONITOR_URL,
     SMALL_MONITOR_URL,
     BOT_TOKEN
@@ -21,6 +23,13 @@ app.listen(PORT, () => {
     // server status && errors logger
     serverLogger.info(`Server Started in port : ${PORT}!`);
 })
+
+// анонимные логи для пользователей
+setInterval(async function () {
+    await getMonitorStatus(BIG_MONITOR_URL).then(res => historyLogger.info(tagsRemover(res)));
+    await getMonitorStatus(SMALL_MONITOR_URL).then(res => historyLogger.info(tagsRemover(res)));
+}, 600000)
+
 // telegram
 const bot = new Telegraf(BOT_TOKEN);
 
@@ -31,13 +40,15 @@ bot.start((ctx) => {
         '\nОдин раз в десять минут бот заходит на сайт фирменного магазина rushop.lg.com, получает информацию о наличии моделей 24MD4KL-B.AEU, 27MD5KL-B.AEU и присылает актуальный статус в телеграм.\n' +
         '\n<b>Команды:</b>\n' +
         '\n/status — Узнать наличие в данный момент.\n' +
-        '\n/subscribe — Подписаться на получение уведомлений. Сообщения будут приходить один раз в десять минут.\n'
+        '\n/subscribe — Подписаться на получение уведомлений. Сообщения будут приходить один раз в десять минут.\n' +
+        '\n/history — история запросов. <b>ВНИМАНИЕ!</b> Вы получите всю историю запросов (144 запроса в сутки), начиная с 20 февраля 2022 года, по настоящее время.\n'
     );
-    ctx.replyWithPhoto({ url: 'https://ohuel.ru/lg/monitors.jpg' });
+    ctx.replyWithPhoto('https://ohuel.ru/lg/monitors.jpg');
 })
 
 bot.hears('/status', ctx => {
     ctx.reply('Получаю данные...')
+
     async function checkStatus() {
         ctx.replyWithHTML(await getMonitorStatus(BIG_MONITOR_URL)).then(res => monitorsLogger.info(`${res.text}, user: ${res.chat.username}`));
         ctx.replyWithHTML(await getMonitorStatus(SMALL_MONITOR_URL)).then(res => monitorsLogger.info(`${res.text}, user: ${res.chat.username}`));
@@ -53,6 +64,25 @@ bot.hears('/subscribe', ctx => {
         ctx.replyWithHTML(await getMonitorStatus(SMALL_MONITOR_URL)).then(res => monitorsLogger.info(`${res.text}, user: ${res.chat.username}`));
     }, 600000)
 })
+
+bot.hears('/history', ctx => {
+    try {
+        const telegramMessageMaxSize = 4012; // ~ 60 строк файла логов
+        const logs = fs.readFileSync('./logs/history.log', 'utf8');
+        const sliced = logs.length / telegramMessageMaxSize;
+        let start = 0;
+        let end = telegramMessageMaxSize;
+
+        for (let i = 0; i < sliced; i++) {
+            let messageToSend = logs.slice(start, end);
+            start = start + telegramMessageMaxSize;
+            end = end + telegramMessageMaxSize;
+            ctx.reply(messageToSend);
+        }
+    } catch (err) {
+        ctx.reply(`Ошибка чтения файла логов: ${err}`)
+    }
+});
 
 // Запуск бота
 bot.launch();
